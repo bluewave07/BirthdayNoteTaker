688e39483ece7c223e88babceba875068ddc78d3
Abdulkadir Akyurt <akyurt.a@gmx.de>
2024-02-27 20:25:12 +0100

    used UserDefaults method to seve data

--- BirthdayNoteTaker/ViewController.swift
+++ BirthdayNoteTaker/ViewController.swift
@@ -8,11 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var dateField: UITextField!
+    
+    @IBOutlet weak var nameView: UILabel!
+    @IBOutlet weak var dateView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let newName = storedName as? String {
+            nameView.text = "Name : \(newName)"
+        }
+        
+        if let newBirthday = storedBirthday as? String {
+            dateView.text = " Birthday : \(newBirthday)"
+        }
+        
+        
     }
+    
+    @IBAction func saveButton(_ sender: Any) {
+        UserDefaults.standard.set(nameField.text!, forKey: "name")
+        UserDefaults.standard.set(dateField.text!, forKey: "birthday")
+        
+        //UserDefaults.standard.synchronize()
+        
+        
+        nameView.text = "Name : \(nameField.text!)"
+        dateView.text = "Birthday : \(dateField.text!)"
+        
+
+    }
+    
 
 
 }
